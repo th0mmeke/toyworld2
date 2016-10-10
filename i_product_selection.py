cb9dcfd827b194aaa805f3e8366d89513a74621b
Thomas Young <thomas.young@canterbury.ac.nz>
2016-10-10 14:00:32 +1300

    Method documentation

--- i_product_selection.py
+++ i_product_selection.py
@@ -1,5 +1,16 @@
 class IProductSelection(object):
 
     @classmethod
-    def get_products(cls, reactants):
+    def get_products(cls, reactants, chemistry):
+
+        """
+        Return the products of a reaction for a given set of reactants, assuming some particular Chemistry.
+
+        Reactants can be Atoms, Molecules or subclasses of either.
+
+        :param reactants: list of IElement
+        :param chemistry: IChemistry
+        :return: list of IElement
+        """
+
         pass
