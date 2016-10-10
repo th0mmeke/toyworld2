e05c691693d8fccd9d617c5d84515948b6f4e7c5
Thomas Young <thomas.young@canterbury.ac.nz>
2016-10-10 14:01:38 +1300

    Changed design - instead of two ProductSelection implementations, replace by ReactionChemistry with injectable selection method. Makes for easier testing and cleaner implementation.

---
+++ tests/test_reaction_chemistry.py
@@ -0,0 +1,48 @@
+import unittest
+
+from reaction_chemistry import ReactionChemistry
+
+from atom import Atom
+from molecule import Molecule
+from tests.stub_chemistry import StubChemistry
+
+
+class TestReactionChemistry(unittest.TestCase):
+
+    def test_base_get_products(self):
+
+        """
+        Given the use of the StubChemistry
+        When get_products(<Reactants>) is called
+        Then the response is given by <Products>
+
+        | Reactants         | Products      | Notes
+        | Atom('A')         | -             | Raises ValueError
+        | []                | []            | Raises ValueError
+
+        """
+
+        rc = ReactionChemistry(StubChemistry, lambda x: x)
+        with self.assertRaises(ValueError):
+            rc.get_products(Atom('A'))
+        with self.assertRaises(ValueError):
+            rc.get_products([])
+
+    def test_get_products(self):
+
+        """
+        Given the use of the StubChemistry
+        And using <Selection> for the value of the selection parameter
+        When get_products(<Reactants>) is called
+        Then expect <Products>
+
+        | Selection         | Reactants               | Products                            |
+        | lambda x: x       | [Molecule('A')]         | [Molecule('A')]                     |
+
+        """
+
+        rc = ReactionChemistry(StubChemistry, lambda x: x)
+
+        self.assertListEqual(rc.get_products(reactants=[Molecule('F')]), [Molecule('A'), Molecule('B')])
+
+
