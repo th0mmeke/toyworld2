afdbd4c2dee16227b2f951d1323ab992a014b9e4
th0mmeke <tom@hensonyoung.net>
2016-10-11 21:14:02 +1300

    First implementation of a kinetic molecule leveraging PyMunk and RDKit

---
+++ tests/test_kinetic_molecule.py
@@ -0,0 +1,65 @@
+import unittest
+
+from rdkit.Chem import AllChem as Chem
+from rdkit.rdBase import DisableLog, EnableLog
+from kinetic_molecule import KineticMolecule
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        DisableLog('rdApp.error')
+
+    def tearDown(self):
+        EnableLog('rdApp.error')
+
+    def testInit(self):
+        mol = KineticMolecule("O=C=O", internal_energy=30)
+        self.assertEqual(3, mol.GetNumAtoms())
+        self.assertEqual(30, mol.get_internal_energy())
+
+        mol = KineticMolecule(Chem.MolFromSmiles("O=C=O"), internal_energy=30)
+        self.assertEqual(3, mol.GetNumAtoms())
+        self.assertEqual(30, mol.get_internal_energy())
+        self.assertEqual(44.009, mol.get_mass())
+
+        with self.assertRaises(ValueError):
+            KineticMolecule(Chem.MolFromSmiles("O"), -10)
+
+        mol = KineticMolecule('[O][H-]')
+        self.assertEqual('[H][O]', Chem.MolToSmiles(mol))  # RDKit converts to this...
+        mol = KineticMolecule('[H+].[OH-]')
+        self.assertEqual('[H+].[H][O-]', Chem.MolToSmiles(mol))  # RDKit converts to this...
+
+    @unittest.skip("Waiting for implementation of a standard Chemistry")
+    def testPotentialEnergy(self):
+        chem = ChemistryFactory.new()
+        self.assertEqual(0, KineticMolecule('[O].[O]').get_potential_energy(chem))
+        self.assertEqual(chem.get_bond_energy(Chem.Atom('O'), Chem.Atom('O'), end_bond_type=2), KineticMolecule('[O]=[O]').get_potential_energy(chem))
+        self.assertTrue(KineticMolecule('[O]=[O]').get_potential_energy(chem) < KineticMolecule('[O].[O]').get_potential_energy(chem))  # bonds have NEGATIVE energy, so adding bonds REDUCES PE
+        O1H_bond = chem.get_bond_energy(Chem.Atom('O'), Chem.Atom('H'), end_bond_type=1)
+        O2O_bond = chem.get_bond_energy(Chem.Atom('O'), Chem.Atom('O'), end_bond_type=2)
+        self.assertEqual(O2O_bond, KineticMolecule('[O]=[O]').get_potential_energy(chem))
+        self.assertEqual(2 * O1H_bond, KineticMolecule('[H]O[H]').get_potential_energy(chem))
+        self.assertEqual(O1H_bond, KineticMolecule('[H+].[OH-]').get_potential_energy(chem))
+
+    def testGetStronglyConnectedComponents(self):
+        mol = KineticMolecule("O=C=O.O")
+        self.assertEqual(6, mol.GetNumAtoms())
+        self.assertEqual([set([0, 1, 2]), set([3, 4, 5])], mol._get_strongly_connected_components())
+        mol = KineticMolecule("[H]")
+        self.assertEqual([[0]], mol._get_strongly_connected_components())
+
+    def testSameComponents(self):
+        mol = KineticMolecule("O=C=O.[H]O[H]")
+        self.assertFalse(mol.same_component(0, 5))
+        self.assertTrue(mol.same_component(0, 1))
+        mol = KineticMolecule("CO.N", components=[set(range(6)), set(range(6, 10))])
+        self.assertTrue(mol.same_component(0, 2))
+        self.assertFalse(mol.same_component(0, 8))
+
+    def testAssignFormalCharge(self):
+        # [H]O[H] -> [OH-]+[H+] Hydroxyl plus proton
+        mol = KineticMolecule('[H].[OH]')
+        mol._assign_formal_charge()
+        self.assertEqual('[H+].[H][O-]', Chem.MolToSmiles(mol))
