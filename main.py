ad8f6961febe7c8681229c83b6ef9d3163144d2b
Thomas Young <thomas.young@canterbury.ac.nz>
2016-10-19 09:42:28 +1300

    Removed the second call to ToyWorld (no longer needed for checking)

--- main.py
+++ main.py
@@ -83,12 +83,7 @@ if __name__ == "__main__":
         for i in range(quantity):
             population.append(Molecule(symbol))
 
-    tw = ToyWorld(reactor=SpatialReactantSelection(population=population, ke=10),
-                  chemistry=SemiRealisticChemistry(bond_energies=bond_energies),
-                  product_selection=least_energy_product_selection.product_selection)
-    state = tw.run(generations=200, state=State(persistence=dummy))
-
-    tw = ToyWorld(reactor=SpatialReactantSelection(population=population, ke=10),
+    tw = ToyWorld(reactor=SpatialReactantSelection(population=population, ke=100),
                   chemistry=SemiRealisticChemistry(bond_energies=bond_energies),
                   product_selection=least_energy_product_selection.product_selection)
     state = tw.run(generations=200, state=State(persistence=dummy))
