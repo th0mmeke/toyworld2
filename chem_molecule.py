77282f88133dc6590966dd9d9e7731653ee4b540
Thomas Young <thomas.young@canterbury.ac.nz>
2016-10-20 09:32:33 +1300

    New subclass of Molecule for semi-realistic chemistries that enforces SMILES

---
+++ chem_molecule.py
@@ -0,0 +1,37 @@
+from molecule import Molecule
+
+from rdkit.Chem import AllChem as Chem
+
+
+class ChemMolecule(Molecule):
+
+    """
+    Molecules are conceptually just combinations of Atoms, described by some character string symbol.
+    The symboll has no particular meaning except to an IChemistry. For example, an IChemistry based on RDKit would expect symbols in SMILES form.
+
+    Any transformations of Molecules is the responsibility of a IChemistry.
+    """
+
+    def __init__(self, symbol):
+        """
+        Initialise a new ChemMolecule represented by character string in SMILES notation.
+
+        :param symbol: SMILES string
+        """
+
+        if not isinstance(symbol, basestring):
+            raise TypeError
+
+        mol = Chem.MolFromSmiles(symbol)
+        if mol is None:
+            raise ValueError
+
+        mol = Chem.AddHs(mol)
+        self.mass = sum([atom.GetMass() for atom in mol.GetAtoms()])
+        self.symbol = Chem.MolToSmiles(mol)
+
+    def get_symbol(self):
+        return self.symbol
+
+    def __str__(self):
+        return self.symbol
---
+++ tests/test_chem_molecule.py
@@ -0,0 +1,25 @@
+import unittest
+
+from chem_molecule import ChemMolecule
+from rdkit.rdBase import DisableLog, EnableLog
+
+class TestMolecule(unittest.TestCase):
+
+    def setUp(self):
+        DisableLog('rdApp.error')
+
+    def tearDown(self):
+        EnableLog('rdApp.error')
+
+    def test_init(self):
+        with self.assertRaises(TypeError):
+            ChemMolecule()
+        with self.assertRaises(TypeError):
+            ChemMolecule(symbol=12)
+        with self.assertRaises(ValueError):
+            ChemMolecule(symbol="AB")
+        self.assertIsInstance(ChemMolecule(symbol="C"), ChemMolecule)
+        self.assertIsInstance(ChemMolecule(symbol="Br"), ChemMolecule)
+
+    def test_add_Hs(self):
+        self.assertEqual(ChemMolecule(symbol="O").get_symbol(), "[H]O[H]")
