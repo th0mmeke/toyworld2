586861e8e98395faa276190fc1bb012f05d46de4
Thomas Young <thomas.young@canterbury.ac.nz>
2016-11-02 16:37:41 +1300

    Start generating standard evolutionary stats from the run records

--- evaluate/test.py
+++ evaluate/test.py
@@ -1,47 +1,50 @@
 import json
 import numpy as np
-import unittest
 
-# state = State(filename="toyworld2.json")
-# for i in range(5000000):
-#     state.add(Reaction([Molecule('A'), Molecule('B')], products=[Molecule('C'), Molecule('D')], reactant_value=i, product_value=i).as_dict())
-# del state
 
-with open('/tmp/toyworld2/toyworld2.json') as f:
-    reactions = json.load(f)
-    print(reactions['initial_population'])
+class Evaluate(object):
 
-    # 1. Long molecules
-    # 2. That are similar to each other
-    # 3. And that in aggregate show a period of exponential growth (assumes A+X->2A+Y rather than A+X->A+Y...)
-    # 3a. n_t+1 approx 2n_t
+    def __init__(self, filename="../data/toyworld2.json"):
 
-    elements = set(reactions['initial_population'])  # COPY initial items
-    for reaction in reactions['reactions']:
-        elements.update(reaction['products'])
-    number_of_unique_elements = len(elements)
-    population = np.zeros((len(reactions['reactions'])+1, number_of_unique_elements), dtype=np.dtype(int))
+        with open(filename) as f:
+            reactions = json.load(f)
 
-    print(len(reactions['reactions']))
-    unique_species = list(elements)
-    for x in reactions['initial_population']:
-        population[0, unique_species.index(x)] += 1
+        elements = set(reactions['initial_population'])  # COPY initial items
+        for reaction in reactions['reactions']:
+            elements.update(reaction['products'])
+        number_of_unique_elements = len(elements)
 
-    i = 1
-    for reaction in reactions['reactions']:
-        population[i, :] = population[i-1, :]
-        for x in reaction['reactants']:
-            population[i, unique_species.index(x)] -= 1
-        for y in reaction['products']:
-            population[i, unique_species.index(y)] += 1
-        i += 1
+        # Population is an array (number_of_generations + 1) x number_of_unique_elements_observed_in_the_population
+        self.population = np.zeros((len(reactions['reactions'])+1, number_of_unique_elements), dtype=np.dtype(int))
 
-    final = np.zeros(number_of_unique_elements, dtype=np.dtype(int))
-    for x in reactions['final_population']:
-        final[unique_species.index(x)] += 1
+        self.unique_species = list(elements)
+        for x in reactions['initial_population']:
+            self.population[0, self.unique_species.index(x)] += 1
 
-    assert list(final) == list(population[len(reactions['reactions']), :])
+        i = 1
+        for reaction in reactions['reactions']:
+            self.population[i, :] = self.population[i-1, :]
+            for x in reaction['reactants']:
+                self.population[i, self.unique_species.index(x)] -= 1
+            for y in reaction['products']:
+                self.population[i, self.unique_species.index(y)] += 1
+            i += 1
 
-    print([x for x in unique_species if len(x) > 10])
-    print(population[:, np.array(map(lambda x: len(x) > 10, unique_species), dtype=bool)])
+        self.final = np.zeros(number_of_unique_elements, dtype=np.dtype(int))
+        for x in reactions['final_population']:
+            self.final[self.unique_species.index(x)] += 1
+
+        assert list(self.final) == list(self.population[len(reactions['reactions']), :])
+
+
+e = Evaluate()
+
+delta_c = e.population[:, np.array(map(lambda x: len(x) > 10, e.unique_species), dtype=bool)]
+
+presence = np.apply_along_axis(lambda x: x > 0, 0, delta_c)
+delta_p = np.apply_along_axis(sum, 0, presence)
+print(delta_p)
+max = np.apply_along_axis(max, 0, longer_elements)
+print(max)
+
 
