bd3feb6b1dece85f590ba0f150851a3fb5b73e4d
th0mmeke <tom@hensonyoung.net>
2016-11-10 08:12:01 +1300

    Move graph write into evaluator

--- evaluate/evaluator_cycles.py
+++ evaluate/evaluator_cycles.py
@@ -1,6 +1,7 @@
 import networkx as nx
-import itertools
+import re
 import collections
+import string
 
 
 class EvaluatorCycles(object):
@@ -77,6 +78,70 @@ class EvaluatorCycles(object):
                 print({'cycle': cycle, 'stoichiometry': stoichiometry})
 
         return reactant_stoichiometry
+
+    def write_subset_to_graphml(self, seed, cycles, path='subgraph.gml'):
+        """
+        Write a subset of the graph containing all cycles that include the molecule seed as reactant or product, replacing full labels by single letter ones that are better suited
+        to graph visualisation. Includes nodes for each molecule that is a reactant or a product in any of these cycles.
+        :param seed: Label of node to mark as node 'a' in the subgraph
+        :param cycles: [[Labels of graph nodes]]
+        :param path: Output filepath
+        :return:
+        """
+
+        re_exp = r'(?<=\d).*?(?=\+\d|>|$)'  # without $: bug >1[H]c1n[c][c]nc([H])[n-][c]n1 should produce [H]c1n[c][c]nc([H])[n-][c]n1, instead produces []
+
+        # Find all nodes to be included in the subgraph - nodes in cycles plus reactant or product molecules
+        nodes = set()
+        for cycle in cycles:
+            for node in cycle:
+                nodes.add(node)
+                if node[-1] == '>' or node[0] == '>':
+                    nodes.update(re.findall(re_exp, node))  # nodes that appear in reactions only
+
+        try:
+            nodes.remove('')  # artifact of the re_exp used
+        except KeyError:
+            pass
+
+        subgraph = self.g.subgraph(nodes)  # Warning: subgraph attributes will change in self.g; nodes/edges however are independent (nx.subgraph() documentation)
+
+        if len(nodes) > len(string.ascii_letters):
+            raise ValueError("Too many nodes for visualisation")
+        else:
+            mapping = {}
+            reaction_mapping = {}
+            letters = list(string.ascii_letters)
+
+            # Label the seed node as 'a'
+            nodes.remove(seed)
+            mapping[seed] = letters.pop(0)
+
+            for node in nodes:
+                if node[-1] != '>' and node[0] != '>':
+                    new_label = letters.pop(0)
+                    mapping[node] = new_label
+
+            for node in nodes:
+                if node[-1] == '>' or node[0] == '>':
+                    # substitute new labels for old in reaction description, and add to mapping
+                    new_label = node
+
+                    for old in re.findall(re_exp, new_label):
+                        if old != '':  # discard these - artifact of the lookahead in the re expression consuming > before matching a molecule that ends in a digit
+                            if old not in mapping.keys():  # reactant from outside the cycle entering part-way through
+                                new_label = letters.pop(0)
+                                mapping[old] = new_label
+                            new_label = string.replace(new_label, old, mapping[old])
+
+                    reaction_mapping[node] = new_label
+
+            mapping.update(reaction_mapping)
+            print(mapping)
+
+            nx.relabel_nodes(subgraph, mapping, copy=False)  # remap labels in-place
+
+        nx.write_graphml(subgraph, path)
 
     @classmethod
     def make_canonical(cls, reactants):
