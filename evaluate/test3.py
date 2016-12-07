ae427caef583cace6404ee310e14458ffb033508
th0mmeke <tom@hensonyoung.net>
2016-12-08 07:01:36 +1300

    Minimal summary of metrics

--- evaluate/test3.py
+++ evaluate/test3.py
@@ -2,19 +2,21 @@ import json
 import os
 
 
-evaldir = "."
-datadir = "..\data"
+evaldir = "C:\Users\Thom\Dropbox/Experiments"
+datadir = "C:\Users\Thom\Dropbox/Experiments"
 
+print("File, No. of reactions, Size of final population")
 for filename in os.listdir(datadir):
     basename, ext = os.path.splitext(filename)
-    if ext == '.json':  # in working directory
+    if ext == '.json' and  (len(basename) > 7 and not basename[-7:] == '-actual'):
         with open(os.path.join(datadir, filename)) as f:
             reactions = json.load(f)
             print(filename, len(reactions['reactions']), len(reactions['final_population']))
 
+print("File, No. of Cycles")
 for filename in os.listdir(evaldir):
     basename, ext = os.path.splitext(filename)
-    if ext == ".json":
-        with open(filename) as f:
+    if ext == '.json' and (len(basename) > 7 and basename[-7:] == '-actual'):
+        with open(os.path.join(evaldir, filename)) as f:
             all_cycles = json.load(f)
             print(filename, len(all_cycles))
