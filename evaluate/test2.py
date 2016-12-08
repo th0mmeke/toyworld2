51b3ad2c3f68188693566fb46afd2e2342d34a14
th0mmeke <tom@hensonyoung.net>
2016-12-09 07:17:28 +1300

    Evaluator for stable states

--- evaluate/test2.py
+++ evaluate/test2.py
@@ -3,12 +3,12 @@ import json
 
 from evaluator_actual_cycles import EvaluatorActualCycles
 
-datadir = "/home/cosc/guest/tjy17/Dropbox/Experiments"
+datadir = "C:\Users\Thom\Dropbox/Experiments"
 
-for filename in sorted(os.listdir(datadir)):
+for filename in sorted(os.listdir(datadir), reverse=True):
     basename, ext = os.path.splitext(filename)
-    evaluator_filename = os.path.join(datadir, "{}-actual.json".format(basename))
-    if ext == '.json' and not os.path.exists(evaluator_filename) and (len(basename) > 7 and not basename[-7:] == '-actual'):
+    evaluator_filename = os.path.join(datadir, "{}-potential.json".format(basename))
+    if ext == '.json' and not os.path.exists(evaluator_filename) and basename[-1:] != 'l' and basename[-1:] != 'y':
         data_filename = os.path.join(datadir, filename)
         with open(data_filename) as f:
             state = json.load(f)
@@ -22,8 +22,14 @@ for filename in sorted(os.listdir(datadir)):
             count += 1
             if len(reactant) >= 10:
                 print("{}/{}: {}".format(count, len(e.reactants), reactant))
-                population_stoichiometry.extend(e.get_reactant_stoichiometry(reactant, minimum_stoichiometry=3, max_depth=10))
-        all_cycles = [x['cycle'] for x in population_stoichiometry]
+                s = e.get_reactant_stoichiometry(reactant, minimum_stoichiometry=2, max_depth=10)
+                for item in s:
+                    cycle = []
+                    for step in item['cycle']:
+                        if '+' not in step and '>' not in step and '<' not in step:
+                            cycle.append(e.get_smiles(step))
+                    print(item['cycle'], cycle)
+                    population_stoichiometry.append({'stoichiometry': item['stoichiometry'], 'cycle': cycle})
 
         with open(evaluator_filename, mode='w') as f:
-            json.dump(all_cycles, f)
+            json.dump(population_stoichiometry, f)
--- evaluate/test3.py
+++ evaluate/test3.py
@@ -1,22 +1,31 @@
 import json
 import os
+import collections
 
 
 evaldir = "C:\Users\Thom\Dropbox/Experiments"
 datadir = "C:\Users\Thom\Dropbox/Experiments"
 
-print("File, No. of reactions, Size of final population")
-for filename in os.listdir(datadir):
-    basename, ext = os.path.splitext(filename)
-    if ext == '.json' and  (len(basename) > 7 and not basename[-7:] == '-actual'):
-        with open(os.path.join(datadir, filename)) as f:
-            reactions = json.load(f)
-            print(filename, len(reactions['reactions']), len(reactions['final_population']))
+
 
 print("File, No. of Cycles")
 for filename in os.listdir(evaldir):
     basename, ext = os.path.splitext(filename)
-    if ext == '.json' and (len(basename) > 7 and basename[-7:] == '-actual'):
+    if ext == '.json' and basename[-3:] == 'ial':
         with open(os.path.join(evaldir, filename)) as f:
+            stable_states = collections.defaultdict(int)
             all_cycles = json.load(f)
             print(filename, len(all_cycles))
+            for cycle in all_cycles:
+                #print(cycle['stoichiometry'], cycle['cycle'])
+                stable_states[cycle['cycle'][0]] += 1
+
+            print(sum(stable_states.values())/len(stable_states), stable_states)
+
+# print("File, No. of Cycles")
+# for filename in os.listdir(evaldir):
+#     basename, ext = os.path.splitext(filename)
+#     if ext == '.json' and (len(basename) > 7 and basename[-7:] == '-actual'):
+#         with open(os.path.join(evaldir, filename)) as f:
+#             all_cycles = json.load(f)
+#             print(filename, len(all_cycles))
