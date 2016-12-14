3a951374fc1016f85e74d3084f15d85d51eadb5e
th0mmeke <tom@hensonyoung.net>
2016-12-15 08:15:47 +1300

    Move evaluation into method

--- evaluate/test2.py
+++ evaluate/test2.py
@@ -3,13 +3,15 @@ import json
 
 from evaluator_actual_cycles import EvaluatorActualCycles
 
-datadir = "C:\Users\Thom\Dropbox/Experiments"
 
-for filename in sorted(os.listdir(datadir), reverse=True):
+
+def evaluate(filename, datadir="C:\Users\Thom\Dropbox/Experiments"):
     basename, ext = os.path.splitext(filename)
-    evaluator_filename = os.path.join(datadir, "{}-potential.json".format(basename))
+    evaluator_filename = os.path.join(datadir, "{}-actual.json".format(basename))
+
     if ext == '.json' and not os.path.exists(evaluator_filename) and basename[-1:] != 'l' and basename[-1:] != 'y':
         data_filename = os.path.join(datadir, filename)
+        print(data_filename)
         with open(data_filename) as f:
             state = json.load(f)
         e = EvaluatorActualCycles(reactions=state['reactions'])
@@ -33,3 +35,8 @@ for filename in sorted(os.listdir(datadir), reverse=True):
 
         with open(evaluator_filename, mode='w') as f:
             json.dump(population_stoichiometry, f)
+
+datadir = "C:\Users\Thom\Dropbox/Experiments"
+# evaluate('1480963448-0-21-0.json')
+for filename in sorted(os.listdir(datadir), reverse=True):
+    evaluate(filename)
