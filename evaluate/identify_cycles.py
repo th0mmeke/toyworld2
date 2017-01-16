6b7f6f93aa5e3a737ccb1b2905a39065d19c0df0
th0mmeke <tom@hensonyoung.net>
2017-01-17 07:37:17 +1300

    Just use glob

--- evaluate/identify_cycles.py
+++ evaluate/identify_cycles.py
@@ -8,40 +8,39 @@ from evaluator_actual_cycles import EvaluatorActualCycles
 def evaluate(filename, datadir):
     basename, ext = os.path.splitext(filename)
 
-    if ext == '.json' and basename[-1:] in string.digits:
 
-        for evaluator in ['actual', 'potential']:
-            evaluator_filename = os.path.join(datadir, '{}-{}.json'.format(basename, evaluator))
-            if not os.path.exists(evaluator_filename):
+    for evaluator in ['actual', 'potential']:
+        evaluator_filename = os.path.join(datadir, '{}-{}.json'.format(basename, evaluator))
+        if not os.path.exists(evaluator_filename):
 
-                data_filename = os.path.join(datadir, filename)
-                print(data_filename, evaluator_filename)
-                with open(data_filename) as f:
-                    state = json.load(f)
-                e = EvaluatorActualCycles(reactions=state['reactions'][:10000])
+            data_filename = os.path.join(datadir, filename)
+            print(data_filename, evaluator_filename)
+            with open(data_filename) as f:
+                state = json.load(f)
+            e = EvaluatorActualCycles(reactions=state['reactions'][:10000])
 
-                # t = {x: len(e.smiles[x]) for x in e.reactants}
-                # import operator
-                # print(sorted(t.items(), key=o
+            # t = {x: len(e.smiles[x]) for x in e.reactants}
+            # import operator
+            # print(sorted(t.items(), key=o
 
-                population_stoichiometry = []
-                count = 0
-                for reactant in e.reactants:
-                    count += 1
-                    if evaluator != 'potential' or len(reactant) >= 10:
-                        print("{}/{}: {}".format(count, len(e.reactants), reactant))
-                        s = e.get_reactant_stoichiometry(reactant, minimum_stoichiometry=2, max_depth=10)
-                        for item in s:  # item = {'cycle':cycle, 'stoichiometry': stoichiometry}
-                            if evaluator == 'potential':  # replace id with smiles
-                                cycle = []
-                                for step in item['cycle']:
-                                    if '+' not in step and '>' not in step and '<' not in step:
-                                        cycle.append(e.get_smiles(step))
-                                item['cycle'] = cycle
-                            population_stoichiometry.append({'stoichiometry': item['stoichiometry'], 'cycle': item['cycle']})
+            population_stoichiometry = []
+            count = 0
+            for reactant in e.reactants:
+                count += 1
+                if evaluator != 'potential' or len(reactant) >= 10:
+                    print("{}/{}: {}".format(count, len(e.reactants), reactant))
+                    s = e.get_reactant_stoichiometry(reactant, minimum_stoichiometry=2, max_depth=10)
+                    for item in s:  # item = {'cycle':cycle, 'stoichiometry': stoichiometry}
+                        if evaluator == 'potential':  # replace id with smiles
+                            cycle = []
+                            for step in item['cycle']:
+                                if '+' not in step and '>' not in step and '<' not in step:
+                                    cycle.append(e.get_smiles(step))
+                            item['cycle'] = cycle
+                        population_stoichiometry.append({'stoichiometry': item['stoichiometry'], 'cycle': item['cycle']})
 
-                with open(evaluator_filename, mode='w') as f:
-                    json.dump(population_stoichiometry, f)
+            with open(evaluator_filename, mode='w') as f:
+                json.dump(population_stoichiometry, f)
 
 datadir = 'C:\Users\Thom\Dropbox/Experiments'
 if not os.path.isdir(datadir):
@@ -50,5 +49,6 @@ if not os.path.isdir(datadir):
 # evaluate('1481398302-0-19-0.json', datadir)
 
 import glob
-for filename in glob.glob(os.path.join(datadir, '1484291284*.json')):
+for filename in glob.glob(os.path.join(datadir, '1484540618*.json')):
+    print(filename)
     evaluate(filename, datadir)
