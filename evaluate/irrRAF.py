c30ba745ba32dcdf4075d7660de0c9bf02b74842
th0mmeke <tom@hensonyoung.net>
2017-01-18 07:38:24 +1300

    Write irrRAF to json

--- evaluate/irrRAF.py
+++ evaluate/irrRAF.py
@@ -3,12 +3,13 @@ import os
 import glob
 import random
 
-from evaluator_cycles import IdentifySpeciesCycles
+from identify_species_cycles import IdentifySpeciesCycles
 
 datadir = 'C:\Users\Thom\Dropbox/Experiments'
 if not os.path.isdir(datadir):
     datadir = '/home/cosc/guest/tjy17/Dropbox/Experiments'
 
+N_IRRRAF = 50
 
 def compute_closure(g, foodset):
     closure = set(foodset[:])
@@ -60,31 +61,37 @@ def RAF(e, foodset):
 
 def irrRAF(e, foodset):
     #  irrRAF algorithm
-    reactions_a = get_reactions(e)
 
-    print("Original", len([node for node in e.nodes() if not IdentifySpeciesCycles.is_reaction(node)]))
+    g = e.copy()  # don't modify original graph
+    reactions_a = get_reactions(g)
+    random.shuffle(reactions_a)
 
-    for i in range(0, 10):
-        g = e.copy()
-        random.shuffle(reactions_a)
-        for reaction in reactions_a:
-            if g.has_node(reaction):  # case where reduced graph no longer contains this reaction from original graph
-                g_copy = g.copy()
-                g_copy.remove_node(reaction)
-                sub_raf = RAF(g_copy, foodset)
-                if sub_raf.number_of_nodes() > 0:
-                    g = sub_raf
-        irrRAF = [node for node in g.nodes() if not IdentifySpeciesCycles.is_reaction(node)]
-        print("irrRAF", len(irrRAF))
+    for reaction in reactions_a:
+        if g.has_node(reaction):  # case where reduced graph no longer contains this reaction from original graph
+            g_copy = g.copy()
+            g_copy.remove_node(reaction)
+            sub_raf = RAF(g_copy, foodset)
+            if sub_raf.number_of_nodes() > 0:
+                g = sub_raf
 
-for filename in glob.glob(os.path.join(datadir, '1484540618-0-*-selection.json')):
-#for filename in glob.glob(os.path.join(datadir, '1484617345-0-0-selection.json')):
+    return [node for node in g.nodes() if not IdentifySpeciesCycles.is_reaction(node)]
+
+
+#for filename in glob.glob(os.path.join(datadir, '1484540618-0-*-selection.json')):
+for filename in glob.glob(os.path.join(datadir, '1484617345-0-0-selection.json')):
 
     print(filename)
     with open(filename) as f:
         state = json.load(f)
-    foodset = state['initial_population'].keys()  # foodset is 'source' nodes for graph e
 
     e = IdentifySpeciesCycles(reactions=state['reactions'])
 
-    irrRAF(e.g, foodset)
+    basename, ext = os.path.splitext(filename)
+    evaluator_filename = os.path.join(datadir, '{}-irrraf.json'.format(basename))
+
+    foodset = state['initial_population'].keys()  # foodset is 'source' nodes for graph e
+    with open(evaluator_filename, mode='w') as f:
+        for i in range(0, N_IRRRAF):
+            print("{}/{}".format(i, N_IRRRAF))
+            mols_in_irrRAF = irrRAF(e.g, foodset)
+            json.dump(mols_in_irrRAF, f)
