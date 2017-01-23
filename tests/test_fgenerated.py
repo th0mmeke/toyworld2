9c9a50c2adadc18db5d5bffc15d4ee47e9722d41
Thomas Young <thomas.young@canterbury.ac.nz>
2017-01-23 14:18:14 +1300

    Unit tests for fgenerated

---
+++ tests/test_fgenerated.py
@@ -0,0 +1,89 @@
+import unittest
+import networkx as nx
+
+from identify_species_cycles import IdentifySpeciesCycles
+import fgenerated
+
+class TestFgenerated(unittest.TestCase):
+
+    def test_get_reactions(self):
+        reactions = [
+            {'reactants': {'1': 'a'}, 'products': {'2': 'b', '3': 'b'}},
+            {'reactants': {'3': 'b'}, 'products': {'4': 'c'}},
+            {'reactants': {'4': 'c'}, 'products': {'5': 'a'}},
+            {'reactants': {'5': 'a'}, 'products': {'6': 'e'}},
+            {'reactants': {'6': 'e'}, 'products': {'7': 'a', '8': 'a'}},
+        ]
+        e = IdentifySpeciesCycles(reactions=reactions)
+
+        self.assertItemsEqual(['>1c', '>1e', '>1a', '>2b', '>2a'], fgenerated.get_reactions(e.g))
+        self.assertItemsEqual(['1a>', '1b>', '1c>', '1e>'], fgenerated.get_reactions_b(e.g))
+
+    def test_get_RAF(self):
+        reactions = [
+            {'reactants': {'1': 'a', '2': 'b'}, 'products': {'3': 'c'}},
+            {'reactants': {'3': 'c', '5': 'b'}, 'products': {'6': 'd'}}
+        ]
+        e = IdentifySpeciesCycles(reactions=reactions)
+        self.assertTrue(nx.is_isomorphic(e.g, fgenerated.get_fgenerated(e.g, ['a', 'b'])))  # small network has single RAF
+
+        # Steel et al 2013 fig 2b - F-generated
+        foodset = ['f1', 'f2', 'f3', 'f4', 'f5']
+        reactions = [
+            {'reactants': {'1': 'f1', '2': 'p3'}, 'products': {'3': 'p1', '4': 'p4'}},  # r1
+            {'reactants': {'5': 'f2', '6': 'p1'}, 'products': {'7': 'p2'}},  # r2
+            {'reactants': {'8': 'f3', '9': 'p2'}, 'products': {'10': 'p3'}},  # r3
+            {'reactants': {'11': 'f4', '12': 'f5'}, 'products': {'13': 'p1'}},  # r4
+        ]
+        e = IdentifySpeciesCycles(reactions=reactions)
+        expected_nodes = [node for node in e.g.nodes() if not IdentifySpeciesCycles.is_reaction(node)]
+        raf = fgenerated.get_fgenerated(e.g, foodset)
+        self.assertItemsEqual(expected_nodes, list(fgenerated.compute_closure(raf, foodset)))
+        actual_nodes = [node for node in raf if not IdentifySpeciesCycles.is_reaction(node)]
+        self.assertListEqual(expected_nodes, actual_nodes)
+
+        # Steel et al 2013 fig 2a - not F-generated
+        foodset = ['f1', 'f2']
+        reactions = [
+            {'reactants': {'1': 'f1', '2': 'p3'}, 'products': {'3': 'p1', '4': 'p4'}},  # r1
+            {'reactants': {'5': 'f2', '6': 'p1'}, 'products': {'7': 'p2'}},  # r2
+            {'reactants': {'8': 'f3', '9': 'p2'}, 'products': {'10': 'p3'}},  # r3
+        ]
+        e = IdentifySpeciesCycles(reactions=reactions)
+
+        raf = fgenerated.get_fgenerated(e.g, foodset)
+        self.assertItemsEqual(foodset, list(fgenerated.compute_closure(raf, foodset)))
+
+    def test_get_irrRAF(self):
+        """
+        Based on Fig 2 Hordijk and Steel 2004
+        """
+        reactions = [
+            {'reactants': {'1': 'a', '2': 'b'}, 'products': {'3': 'c'}},
+            {'reactants': {'3': 'c', '5': 'b'}, 'products': {'6': 'd'}}
+        ]
+        e = IdentifySpeciesCycles(reactions=reactions)
+        expected_nodes = ['a', 'b', 'c']  # d is not necessary as no catalysis in our F-generated sets
+        print(fgenerated.get_irr_fgenerated(e.g, ['a', 'b']))
+        self.assertItemsEqual(expected_nodes, fgenerated.get_irr_fgenerated(e.g, ['a', 'b']))  # small network has single RAF and hence irrRAF
+
+    def test_reduction_to_irrRAF(self):
+        """
+        Based on fig 2 from Hordijk and Steel 2004
+        """
+        foodset = ['a', 'b']
+
+        reactions = [
+            {'reactants': {'1': 'a', '2': 'b'}, 'products': {'3': 'c'}},
+            {'reactants': {'3': 'c', '5': 'b'}, 'products': {'6': 'd'}},
+            {'reactants': {'11': 'a', '12': 'b'}, 'products': {'13': 'f'}},
+            {'reactants': {'13': 'f', '15': 'b'}, 'products': {'16': 'g'}}
+        ]
+
+        e = IdentifySpeciesCycles(reactions=reactions)
+        expected_nodes = [node for node in e.g.nodes() if not IdentifySpeciesCycles.is_reaction(node)]
+
+        for i in range(0, 20):
+            irr = fgenerated.get_irr_fgenerated(e.g, foodset)
+            self.assertEqual(3, len(irr))
+            self.assertTrue(irr == set(['a', 'b', 'c']) or irr == set(['a', 'b', 'f']))
