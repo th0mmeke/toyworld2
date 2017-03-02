b876da822b74566299d70e2ade78591f9587d45b
th0mmeke <tom@hensonyoung.net>
2017-03-03 07:49:49 +1300

    Naming for consistency

--- deprecated/analyse_multipliers.py
+++ deprecated/analyse_multipliers.py
@@ -1,18 +1,18 @@
 import json
 import os
+from collections import defaultdict
 import glob
+
 import cycle_utilities
+
 
 
 datadir = 'C:\Users\Thom\Dropbox/Experiments'
 if not os.path.isdir(datadir):
     datadir = '/home/cosc/guest/tjy17/Dropbox/Experiments'
-# filebase = '1484540618'
-# filebase = '1484617345'
-
 filebase = '1487535886'
-#filebase = '1481939843'
 
+# Construct list of stable cycles per environment
 
 for filename in glob.glob(os.path.join(datadir, filebase+'*molecules.json')):
 
@@ -28,12 +28,9 @@ for filename in glob.glob(os.path.join(datadir, filebase+'*molecules.json')):
         else:
             with open(os.path.join(datadir, '{}.json'.format(source_basename))) as f:
                 state = json.load(f)
-                smiles = cycle_utilities.load_smiles(state['reactions'])
-
-            species = cycle_utilities.discover_species(all_cycles, smiles)
-
-            autocatalytic = cycle_utilities.discover_autocatalysis(species.itervalues())
-
-            evaluator_filename = os.path.join(datadir, '{}-autocatalytic.json'.format(source_basename))
+            smiles = cycle_utilities.load_smiles(state['reactions'])
+            stable_states, seeds = cycle_utilities.discover_multipliers(all_cycles, smiles)  # [[counts per cycle type]] for this file
+            print(stable_states)
+            evaluator_filename = os.path.join(datadir, '{}-states.json'.format(source_basename))
             with open(evaluator_filename, mode='w') as f:
-                json.dump(autocatalytic, f)
+                json.dump(stable_states, f)
