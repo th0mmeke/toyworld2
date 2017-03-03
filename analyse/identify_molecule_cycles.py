8d0dd61a10bdfc95c04dcfd26ababbc48d5a6f4d
th0mmeke <tom@hensonyoung.net>
2017-03-04 10:20:18 +1300

    Sampling on reactant molecules

--- analyse/identify_molecule_cycles.py
+++ analyse/identify_molecule_cycles.py
@@ -22,12 +22,15 @@ class IdentifyMoleculeCycles(IdentifySpeciesCycles):
         """
 
         self.g = nx.DiGraph()
-        self.reactants = set()
+        self.reactants = set()  # set of smiles
+        self.reactant_molecules = set()  # set of molecule ids
         self.smiles = defaultdict(set)  # smiles[x] = [all ids with smiles of x]
 
+        print("Reactions=", len(reactions))
         for reaction in reactions:
 
             self.reactants.update(reaction['reactants'].values())
+            self.reactant_molecules.update(reaction['reactants'].keys())
 
             canonical_reactants = self.make_canonical(reaction['reactants'].keys()) + '>'
             canonical_products = '>' + self.make_canonical(reaction['products'].keys())
@@ -49,6 +52,41 @@ class IdentifyMoleculeCycles(IdentifySpeciesCycles):
                 self.smiles[smiles].add(id)
                 if not self.g.has_edge(canonical_products, id):
                     self.g.add_edge(canonical_products, id, stoichiometry=product_stoichiometry[smiles])
+
+    def sample_reactant_stoichiometry(self, molecule, minimum_stoichiometry=0, max_depth=15):
+        """
+        Find all unique cycles that include each node that has the SMILES of acs_seed.
+
+        :param acs_seed:
+        :param minimum_stoichiometry:
+        :param max_depth:
+        :return: [{'cycle':cycle, 'stoichiometry':stoichiometry}]
+        """
+        reactant_stoichiometry = []
+
+        #  find all cycles - each will be unique as from unique seed
+        cycles = list(self.find_shortest_paths(self.g, molecule, self.g.node[molecule]['smiles'], max_depth))
+
+        for cycle in cycles:
+            stoichiometry = 1
+            for start_node, end_node in zip(cycle[0:-1], cycle[1:]):
+                if self.is_reaction(start_node) and not self.is_reaction(end_node):  # end_node is product
+                    stoichiometry *= float(self.g[start_node][end_node]['stoichiometry'])
+                if self.is_reaction(end_node) and not self.is_reaction(start_node):  # start_node must be reactant
+                    stoichiometry /= float(self.g[start_node][end_node]['stoichiometry'])
+
+            if stoichiometry >= minimum_stoichiometry:
+                #  first make sure don't already have this cycle to a different end molecule of the same species
+                unique = True
+                for candidate_cycle in reactant_stoichiometry:
+                    x = candidate_cycle['cycle']
+                    if self.g.node[x[0]]['smiles'] == self.g.node[cycle[0]]['smiles'] and x[1:-1] == cycle[1:-1] and self.g.node[x[-1]]['smiles'] == self.g.node[cycle[-1]]['smiles']:
+                        unique = False
+                        break
+                if unique:
+                    reactant_stoichiometry.append({'cycle': cycle, 'stoichiometry': stoichiometry})
+
+        return reactant_stoichiometry
 
     def get_reactant_stoichiometry(self, acs_seed, minimum_stoichiometry=0, max_depth=5):
         """
