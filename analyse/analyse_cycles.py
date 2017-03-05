c5dc0ffa3f7de002a34cd850c8b3be315a1cb733
th0mmeke <tom@hensonyoung.net>
2017-03-05 03:30:44 +0000

    Tidy of analysers

--- analyse/analyse_cycles.py
+++ analyse/analyse_cycles.py
@@ -1,6 +1,7 @@
 import os
 import json
 import random
+import glob
 
 from identify_molecule_cycles import IdentifyMoleculeCycles
 
@@ -12,7 +13,6 @@ def evaluate(filename, datadir):
     evaluator_filename = os.path.join(datadir, '{}-{}sample.json'.format(basename, evaluator))
     data_filename = os.path.join(datadir, filename)
     population_stoichiometry = []
-    test = []
 
     with open(data_filename) as f:
         state = json.load(f)
@@ -25,23 +25,14 @@ def evaluate(filename, datadir):
         sample = random.sample(e.reactant_molecules, len(e.reactant_molecules)/10)
         for mol in sample:
             for item in e.sample_reactant_stoichiometry(mol):
-                [p.append({'stoichiometry': item['stoichiometry'], 'cycle': item['cycle']})
-                test.append(item)
-        print(len(test), len(population_stoichiometry))
+                population_stoichiometry.append(item)
+        print(len(population_stoichiometry))
 
-    # with open(evaluator_filename, mode='w') as f:
-    #     json.dump(population_stoichiometry, f)
+    with open(evaluator_filename, mode='w') as f:
+        json.dump(population_stoichiometry, f)
 
 
-datadir = 'C:\Users\Thom\Dropbox/Experiments'
-if not os.path.isdir(datadir):
-    datadir = '/home/cosc/guest/tjy17/Dropbox/Experiments'
-datadir='..'
-# analyse('1481398302-0-19-0.json', datadir)
-
-import glob
-x = os.path.join('/Users/tjy17/Workspace/toyworld2', '1488595072*.json')
-print(x)
-for filename in glob.glob(x):
+datadir = '/Users/Thom/Workspace/toyworld2'
+for filename in glob.glob(os.path.join(datadir, '1487535886*.json')):
     print(filename)
     evaluate(filename, datadir)
--- analyse/analyse_multipliers.py
+++ analyse/analyse_multipliers.py
@@ -4,22 +4,16 @@ import glob
 import cycle_utilities
 
 
-datadir = 'C:\Users\Thom\Dropbox/Experiments'
-if not os.path.isdir(datadir):
-    datadir = '/home/cosc/guest/tjy17/Dropbox/Experiments'
-# filebase = '1484540618'
-# filebase = '1484617345'
-
-filebase = '1487535886'
-filebase = '1481939843'
+datadir = '/Users/Thom/Workspace/toyworld2'
+filebase = '1488600998'
 
 
-for filename in glob.glob(os.path.join(datadir, filebase+'*molecules.json')):
+for filename in glob.glob(os.path.join(datadir, filebase+'*sample.json')):
 
     basename, ext = os.path.splitext(filename)
     print(filename)
     nc = basename.split('-')
-    source_basename = '{}-{}-{}-{}-{}'.format(filebase, nc[1], nc[2], nc[3], nc[4])
+    source_basename = '{}-{}-{}-{}'.format(filebase, nc[1], nc[2], nc[3])
     with open(os.path.join(datadir, filename)) as f:
         try:
             all_cycles = json.load(f)
@@ -31,6 +25,7 @@ for filename in glob.glob(os.path.join(datadir, filebase+'*molecules.json')):
                 smiles = cycle_utilities.load_smiles(state['reactions'])
 
             species = cycle_utilities.discover_species(all_cycles, smiles)
+            print("Species=", len(species))
 
             multipliers = cycle_utilities.discover_multipliers(species.itervalues())
 
