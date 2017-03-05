0fbde825b072fdc429856931a738b3fd43eaf164
th0mmeke <tom@hensonyoung.net>
2017-03-06 06:03:58 +1300

    Sample cycles multiple times to collect statistical power

--- analyse/analyse_cycles.py
+++ analyse/analyse_cycles.py
@@ -12,7 +12,7 @@ def evaluate(filename, datadir):
     evaluator = 'molecules'
     evaluator_filename = os.path.join(datadir, '{}-{}sample.json'.format(basename, evaluator))
     data_filename = os.path.join(datadir, filename)
-    population_stoichiometry = []
+    counts = []
 
     with open(data_filename) as f:
         state = json.load(f)
@@ -22,17 +22,24 @@ def evaluate(filename, datadir):
         pass
     else:
         print(len(e.reactants), len(e.reactant_molecules))
-        sample = random.sample(e.reactant_molecules, len(e.reactant_molecules)/10)
-        for mol in sample:
-            for item in e.sample_reactant_stoichiometry(mol):
-                population_stoichiometry.append(item)
-        print(len(population_stoichiometry))
 
-    with open(evaluator_filename, mode='w') as f:
-        json.dump(population_stoichiometry, f)
+        for count in range(0, 10):
+            population_stoichiometry = []
+            sample = random.sample(e.reactant_molecules, len(e.reactant_molecules)/10)
+            for mol in sample:
+                for item in e.sample_reactant_stoichiometry(mol):
+                    population_stoichiometry.append(item)
+            counts.append(str(len(population_stoichiometry)))
 
+        with open(evaluator_filename, mode='w') as f:
+            json.dump(population_stoichiometry, f)
+
+    return counts
 
 datadir = '/Users/Thom/Workspace/toyworld2'
-for filename in glob.glob(os.path.join(datadir, '1487535886*.json')):
-    print(filename)
-    evaluate(filename, datadir)
+with open('sample_count.csv', mode='w') as f_counts:
+    for filename in glob.glob(os.path.join(datadir, '1488600998*.json')):
+        print(filename)
+        counts = evaluate(filename, datadir)
+        print(counts)
+        f_counts.write(",".join(counts) + "\n")
