9a73fcd021ec7c6cc74f5bea0c93cb8b47eb997f
th0mmeke <tom@hensonyoung.net>
2017-03-09 16:42:30 +1300

    More useful metadata in multipliers summary

--- analyse/summarise_multipliers.py
+++ analyse/summarise_multipliers.py
@@ -5,35 +5,35 @@ import csv
 
 
 def get_metrics(filename):
-    s_reactant = {}
-    s_product = {}
-    target = {}
-    shape = {}
+    metadata = {}
 
-    experiment = None
+    experiment = -1
     with open(filename, 'rb') as csvfile:
         r = csv.reader(csvfile, delimiter=',')
+        next(r)  # skip header
         for row in r:
-            if experiment is None or int(row[0]) != experiment:
+            if row[0] != experiment:
                 experiment = row[0]
-                s_reactant[experiment] = row[1]
-                s_product[experiment] = row[2]
-                target[experiment] = row[3]
-                shape[experiment] = row[4]
+                environment = 0
+                metadata[experiment] = {}
+            metadata[experiment][str(environment)] = {'s_reactant': row[1], 's_product': row[2], 'target': row[3], 'shape': row[4], 'dfa': row[7], 'sampen': row[8]}
+            environment += 1
 
-    return s_reactant, s_product, target, shape
+    return metadata
 
 
 datadir = '/home/cosc/guest/tjy17/Dropbox/Experiments'
+datadir = '/Users/Thom/Dropbox/Experiments'
 filebase = '1488846568'
+
 metadata_filename = os.path.join(datadir, filebase + "-metadata.csv")
 evaluator_filename = os.path.join(datadir, 'multipliers.csv')
 
-s_reactant, s_product, target, shape = get_metrics(metadata_filename)
+metadata = get_metrics(metadata_filename)
 
 with open(evaluator_filename, mode='w') as f:
 
-    f.write("Datetime, Experiment, Environment, Replicate, S_Reactant, S_Product, Target, Shape, Multipliers\n")
+    f.write("Dataset, Experiment, Environment, Replicate, S_Reactant, S_Product, Target, Shape, DFA, Sample Entropy, Multipliers\n")
 
     for data_filepath in sorted(glob.glob(os.path.join(datadir, filebase+'*multipliers.json'))):
 
@@ -45,5 +45,8 @@ with open(evaluator_filename, mode='w') as f:
             else:
                 filename = os.path.splitext(os.path.basename(data_filepath))[0]
                 nc = filename.split('-')
-                s = ','.join([filebase, nc[1], nc[2], nc[3], s_reactant[nc[1]], s_product[nc[1]], target[nc[1]], shape[nc[1]], str(len(clusters))])
+
+                data = metadata[nc[1]][nc[2]]
+                s = ','.join([filebase, nc[1], nc[2], nc[3], data['s_reactant'], data['s_product'], data['target'], data['shape'], data['dfa'], data['sampen'], str(len(clusters))])
+                print(s)
                 f.write(s + "\n")
