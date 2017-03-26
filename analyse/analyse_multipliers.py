bce8dc9f9a2ddbc8ede1ac863f6d8e9e5b3ee29f
th0mmeke <tom@hensonyoung.net>
2017-03-27 07:56:55 +1300

    Should find cycles in evaldir not datadir after moving directories

--- analyse/analyse_multipliers.py
+++ analyse/analyse_multipliers.py
@@ -22,18 +22,16 @@ def evaluate(experiment_filepath, data_filepath, evaluator_filepath):
             with open(evaluator_filepath, mode='w') as f:
                 json.dump(multipliers, f)
 
-datadir = '/Users/Thom/Dropbox/Experiments'
+datadir = 'C:\Users\Thom\Dropbox\Experiments'
+evaldir = 'C:\Users\Thom\Dropbox\Experiments\sampled-at-0.05'
 
-# /home/cosc/guest/tjy17/Dropbox/Experiments/1488820321-0-4-0-cycles.json
-# /home/cosc/guest/tjy17/Dropbox/Experiments/1488846568-2-1-0.json
-
-for filepath in sorted(glob.glob(os.path.join(datadir, '1488846568*cycles.json'))):
+for filepath in sorted(glob.glob(os.path.join(evaldir, '*cycles.json'))):
     filename = os.path.splitext(os.path.basename(filepath))[0]
     nc = filename.split('-')
     filebase = '{}-{}-{}-{}'.format(nc[0], nc[1], nc[2], nc[3])
 
-    evaluator_filepath = os.path.join(datadir, filebase + "-multipliers.json")
-    data_filepath = os.path.join(datadir, filebase + "-cycles.json")
+    evaluator_filepath = os.path.join(evaldir, filebase + "-multipliers.json")
+    data_filepath = os.path.join(evaldir, filebase + "-cycles.json")
     experiment_filepath = os.path.join(datadir, filebase + ".json")
 
     if not os.path.exists(evaluator_filepath):
